da7a22b4f57af4b5bedfd888593faacf05be59d2
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-21 16:52:45 -0400

    feat: contructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,11 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(String name) {
+        System.out.println("Construyendo el objecto Doctor");
+        System.out.println("El nombre del doctor asignado es: "  + name);
+    }
+
     // Comportamientos
     public void showName() {
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Doctor myDoctor;
 
-        myDoctor = new Doctor();
+        myDoctor = new Doctor(("Carlos"));
         myDoctor.name = "Carlos Palacios";
         myDoctor.showName();
     }
