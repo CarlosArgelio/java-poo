3e5b5d47ef3bb480dbe4a28407ebcd31f80a52b6
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-21 16:38:13 -0400

    create class & modularizing

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor;
+
+        myDoctor = new Doctor();
+        myDoctor.name = "Carlos Palacios";
+        myDoctor.showName();
+    }
+}
