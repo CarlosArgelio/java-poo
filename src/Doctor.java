edfcc894bb64e67fd27917b6c5f5b55fb8acec0f
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-22 20:56:45 -0400

    feat: add static

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,16 +1,24 @@
 public class Doctor {
-    int id;
+    static int id = 0;
     String name;
     String speciality;
 
-    Doctor(String name) {
+    Doctor() {
         System.out.println("Construyendo el objecto Doctor");
+        id++;
+    }
+    
+    Doctor(String name) {
         System.out.println("El nombre del doctor asignado es: "  + name);
     }
 
     // Comportamientos
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Doctor myDoctor;
-
         myDoctor = new Doctor(("Carlos"));
         myDoctor.name = "Carlos Palacios";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+
+        Doctor myDoctorAnn;
+        myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+        System.out.println(Doctor.id);
     }
 }
