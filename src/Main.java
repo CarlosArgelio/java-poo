5af1fa8c1d056f5c942631515fdbd7161d3a81e9
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-22 21:11:26 -0400

    add static with constanst

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,6 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor;
-        myDoctor = new Doctor(("Carlos"));
-        myDoctor.name = "Carlos Palacios";
-        myDoctor.showName();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
-
-        Doctor myDoctorAnn;
-        myDoctorAnn = new Doctor();
-        myDoctorAnn.showId();
-        System.out.println(Doctor.id);
 
         showMenu();
     }
