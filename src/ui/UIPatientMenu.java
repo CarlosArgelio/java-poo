634a3557c2fa84b8ba955736bf684b09a8738f91
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-06-10 13:51:51 -0400

    call showPatientMyAppointments

--- src/ui/UIPatientMenu.java
+++ src/ui/UIPatientMenu.java
@@ -26,6 +26,7 @@ public class UIPatientMenu {
                     showBookAppointmentMenu();
                     break;
                 case 2:
+                    showPatientMyAppointments();
                     break;
                 case 0:
                     UIMenu.showMenu();
